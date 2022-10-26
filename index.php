8cde9c280ac7e56e1dd7e11a488298a4e3493e42
Syafirra Novrainnisa <syafirnov@gmail.com>
2022-10-26 23:50:30 +0700

    innitial commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+    echo "Hello World!<br>";
+
+    $nama = "Syafirra Novrainnisa";
+    $umur = 19;
+
+    echo "Nama saya <strong>$nama</strong>, saya berusia $umur tahun.<br>";
+
+    $namaAdik = "Rania";
+    $umurAdik = "10";
+
+    echo "Nama adik saya <strong>$namaAdik</strong>, saya berusia $umurAdik tahun.<br>";
+
+    $selisihUmur = $umur - $umurAdik;
+    echo "selisih usia saya dan adik saya adalah $selisihUmur tahun."
+
+?>
